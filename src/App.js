efe0a13b6d37c5c964980a84421ac3a72a8392b4
MOHAMMED RAFHATH <danarafha@gmail.com>
2023-06-23 15:26:11 +0530

    created basic pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Hotel from "./pages/hotel/Hotel";
+
 function App() {
-  return <div>hello world</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
