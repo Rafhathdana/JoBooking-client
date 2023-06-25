b382066a1a722cd741054e932f6915d38693132c
MOHAMMED RAFHATH <danarafha@gmail.com>
2023-06-25 16:00:57 +0530

    added fonts ,featured property etc

---
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,80 @@
+import "./featuredProperties.css";
+
+const FeaturedProperties = () => {
+  return (
+    <div className="fp">
+      <div className="fpItem">
+        <img
+          src="https://cf2.bstatic.com/xdata/images/hotel/square600/216475546.webp?k=b3803ba3bc1f5c3b481ac390a752a7a3787c58a2b5c5e2081ecdc1c81fc11cd0&o="
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Aparthotel Stare Miasto</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://cf2.bstatic.com/xdata/images/hotel/square600/216475546.webp?k=b3803ba3bc1f5c3b481ac390a752a7a3787c58a2b5c5e2081ecdc1c81fc11cd0&o="
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Aparthotel Stare Miasto</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://cf2.bstatic.com/xdata/images/hotel/square600/216475546.webp?k=b3803ba3bc1f5c3b481ac390a752a7a3787c58a2b5c5e2081ecdc1c81fc11cd0&o="
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Aparthotel Stare Miasto</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://cf2.bstatic.com/xdata/images/hotel/square600/216475546.webp?k=b3803ba3bc1f5c3b481ac390a752a7a3787c58a2b5c5e2081ecdc1c81fc11cd0&o="
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Aparthotel Stare Miasto</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://cf2.bstatic.com/xdata/images/hotel/square600/216475546.webp?k=b3803ba3bc1f5c3b481ac390a752a7a3787c58a2b5c5e2081ecdc1c81fc11cd0&o="
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Aparthotel Stare Miasto</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProperties;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import Featured from "../../components/featured/Featured";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import PropertyList from "../../components/propertyList/PropertyList";
@@ -13,6 +14,8 @@ const Home = () => {
         <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
       </div>
     </div>
   );
