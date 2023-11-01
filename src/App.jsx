9db93d94ee7747694170a91e165cb9d0cfe7558c
MOHAMMED RAFHATH <danarafha@gmail.com>
2023-11-01 13:10:12 +0530

    builded

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Hotel from "./pages/hotel/Hotel";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Flights from "./pages/flight/Flight";
+import WebsiteInProgress from "./pages/WebsiteInProgress";
+// import CarRentals from "./pages/carRent/CarRent";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/">
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="flight" element={<Flights />} />
+        <Route path="hotels" element={<List />} />
+        <Route path="hotels/:id" element={<Hotel />} />
+      </Route>
+      <Route path="*" element={<WebsiteInProgress />} />
+    </Routes>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { SearchContextProvider } from "./context/SearchContext";
 import { AuthContextProvider } from "./context/AuthContext";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <SearchContextProvider>
-        <App />
-      </SearchContextProvider>
-    </AuthContextProvider>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <SearchContextProvider>
+          <App />
+        </SearchContextProvider>
+      </AuthContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
